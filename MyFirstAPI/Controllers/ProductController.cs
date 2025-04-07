e1d369757ab1ba81b26f53ed1e15b95e60dadf99
Outro Lucas <lucasemmanuelrodrigues737@gmail.com>
2025-04-07 11:48:10 -0300

    New controller ProductController, with GET and POST methods

---
+++ MyFirstAPI/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Models;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> _products = new List<Product>
+        {
+            new Product { id = 1, name = "Teclado", price = 150.00M },
+            new Product { id = 2, name = "Mouse"  , price = 90.00M }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return Ok(_products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            Product product = _products.FirstOrDefault(p => p.id == id);
+            if (product == null) 
+                return NotFound("Não foi possível encontrar :(");
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public ActionResult<Product> Post(Product product)
+        {
+            product.id = _products.Max(p => p.id) + 1;
+            _products.Add(product);
+            return CreatedAtAction(nameof(Get), new {id = product.id }, product);
+        }
+    }
+}
--- MyFirstAPI/Models/Product.cs
+++ MyFirstAPI/Models/Product.cs
@@ -3,7 +3,7 @@
     public class Product
     {
         public int id { get; set; }
-        public string name { get; set; }
+        public string? name { get; set; }
         public decimal price { get; set; }
     }
 }
