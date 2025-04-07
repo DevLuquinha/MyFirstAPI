1c7c97831c624e3463dc82bfc245bdaf99663ab7
Outro Lucas <lucasemmanuelrodrigues737@gmail.com>
2025-04-07 11:31:07 -0300

    New controller, Hello Controller, a endpoint that show a Hello World!

---
+++ MyFirstAPI/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World! Luquinha is going well :)");
+        }
+
+        [HttpGet("nome/{nome}")]
+        public IActionResult GetComNome(string nome)
+        {
+            return Ok($"Hello, {nome}! How's going? ");
+        }
+    }
+}
